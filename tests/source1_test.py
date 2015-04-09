c089d117dcf4ea25856024b1e6664dd477182afd
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-04-08 20:08:25 -0700

    Commit a 86% coverage

--- tests/source1_test.py
+++ tests/source1_test.py
@@ -13,3 +13,31 @@ class TestGetTriangleType(TestCase):
     def test_get_triangle_scalene_all_int(self):
         result = get_triangle_type(1, 2, 3)
         self.assertEqual(result, 'scalene')
+
+    def test_get_triangle_isosceles_all_int(self):
+        result = get_triangle_type(1, 1, 2)
+        self.assertEqual(result, 'isosceles')
+
+    def test_get_triangle_invalid_a_equals_0(self):
+        result = get_triangle_type(0, 1, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_triangle_invalid_b_equals_0(self):
+        result = get_triangle_type(1, 0, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_triangle_invalid_c_equals_0(self):
+        result = get_triangle_type(1, 1, 0)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_triangle_equilateral_all_float(self):
+        result = get_triangle_type(1.0, 1.0, 1.0)
+        self.assertEqual(result, 'equilateral')
+
+    def test_get_triangle_invalid_a_is_string(self):
+        result = get_triangle_type('error', 1, 1)
+        self.assertEqual(result, 'invalid')
+
+    def test_get_triangle_equilateral_a_is_dict(self):
+        result = get_triangle_type({0: 1, 1: 1, 2: 1})
+        self.assertEqual(result, 'equilateral')
