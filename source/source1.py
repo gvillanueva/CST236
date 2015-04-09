7c68651d3c8281b94406695c259608be903dc155
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-04-08 20:44:43 -0700

    get_quad_type function/test additions Adds get_quadrilateral_type function and comment documentation. Adds tests for 100% coverate of get_quadrilateral_type.

--- source/source1.py
+++ source/source1.py
@@ -47,3 +47,29 @@ def get_triangle_type(a=0, b=0, c=0):
         return "isosceles"
     else:
         return "scalene"
+
+def get_quadrilateral_type(a=0, b=0, c=0, d=0):
+    """
+    Determine if the given quadrilateral is a rectangle or square
+
+    :param a: line a
+    :type a: float
+
+    :param b: line b
+    :type b: float
+
+    :param c: line c
+    :type c: float
+
+    :param d: line d
+    :type d: float
+
+    :return: "rectangle", "square", or "other_or_invalid"
+    :rtype: str
+    """
+    if a == b and b == c and c == d:
+        return "square"
+    elif (a == b and c == d) or (a == c and b == d) or (a == d and b == c):
+        return "rectangle"
+    else:
+        return "other_or_invalid"
--- tests/source1_test.py
+++ tests/source1_test.py
@@ -2,6 +2,7 @@
 Test for source.source1
 """
 from source.source1 import get_triangle_type
+from source.source1 import get_quadrilateral_type
 from unittest import TestCase
 
 class TestGetTriangleType(TestCase):
@@ -45,3 +46,17 @@ class TestGetTriangleType(TestCase):
     def test_get_triangle_equilateral_a_is_tuple(self):
         result = get_triangle_type([1, 1, 1])
         self.assertEqual(result, 'equilateral')
+
+class  TestGetQuadrilateralType(TestCase):
+
+    def test_get_quadrilateral_square_all_float(self):
+        result = get_quadrilateral_type(1.0, 1.0, 1.0, 1.0)
+        self.assertEqual(result, 'square')
+
+    def test_get_quadrilateral_rectangle_all_float(self):
+        result = get_quadrilateral_type(1.0, 2.0, 1.0, 2.0)
+        self.assertEqual(result, 'rectangle')
+
+    def test_get_quadrilateral_other_or_invalid(self):
+        result = get_quadrilateral_type(0, 2.0, 1.0, 2.0)
+        self.assertEqual(result, 'other_or_invalid')
