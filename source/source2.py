9fc6246c554262dfa0974bcd36172faad8a02b29
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-04-08 23:55:17 -0700

    Adds rhombus/disconnected tests to source2.py Refactors existing tests and code for invalid/other types as well.

--- source/source2.py
+++ source/source2.py
@@ -5,9 +5,9 @@
 The following code determines if a set of 4 sides of a quadrilateral is square, rectangle, or other/invalid
 """
 
-def get_quadrilateral_type(a=0, b=0, c=0, d=0):
+def get_quadrilateral_type(a=0, b=0, c=0, d=0, w=90, x=90, y=90, z=90):
     """
-    Determine if the given quadrilateral is a rectangle or square
+    Determine if the given quadrilateral is a rectangle, square, rhombus, disconnected, or other/invalid
 
     :param a: line a
     :type a: float
@@ -21,12 +21,32 @@ def get_quadrilateral_type(a=0, b=0, c=0, d=0):
     :param d: line d
     :type d: float
 
+    :param w: angle w
+    :type w: float
+
+    :param x: angle x
+    :type x: float
+
+    :param y: angle y
+    :type y: float
+
+    :param z: angle z
+    :type z: float
+
     :return: "rectangle", "square", or "other_or_invalid"
     :rtype: str
     """
-    if a == b and b == c and c == d:
-        return "square"
-    elif (a == b and c == d) or (a == c and b == d) or (a == d and b == c):
-        return "rectangle"
+    if w == 90 and x == 90 and y == 90 and z == 90:
+        if a == b and b == c and c == d:
+            return "square"
+        elif (a == b and c == d) or (a == c and b == d) or (a == d and b == c):
+            return "rectangle"
+        else:
+            return "invalid"
+    elif w + x + y + z == 360:
+        if w == y and x == z and ((w < 90 and x > 90) or (w > 90 and x < 90)) and a == b and b == c and c == d:
+            return "rhombus"
+        else:
+            return "other"
     else:
-        return "other_or_invalid"
+        return "disconnected"
--- tests/source2_test.py
+++ tests/source2_test.py
@@ -14,6 +14,18 @@ class  TestGetQuadrilateralType(TestCase):
         result = get_quadrilateral_type(1.0, 2.0, 1.0, 2.0)
         self.assertEqual(result, 'rectangle')
 
-    def test_get_quadrilateral_other_or_invalid(self):
+    def test_get_quadrilateral_invalid(self):
         result = get_quadrilateral_type(0, 2.0, 1.0, 2.0)
-        self.assertEqual(result, 'other_or_invalid')
+        self.assertEqual(result, 'invalid')
+
+    def test_get_quadrilateral_rhombus(self):
+        result = get_quadrilateral_type(2.0, 2.0, 2.0, 2.0, 30, 150, 30, 150)
+        self.assertEqual(result, 'rhombus')
+
+    def test_get_quadrilateral_disconnect(self):
+        result = get_quadrilateral_type(1.0, 1.0, 1.0, 1.0, 3, 150, 30, 150)
+        self.assertEqual(result, 'disconnected')
+
+    def test_get_quadrilateral_other(self):
+        result = get_quadrilateral_type(216.0, 133.0, 216.0, 133.0, 97, 83, 97, 83)
+        self.assertEqual(result, 'other')
