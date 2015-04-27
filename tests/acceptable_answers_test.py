78fcb0557a9c6efca7d27d94e6d94856e05ee9a3
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-04-27 16:24:39 -0700

    Getting closer

--- tests/acceptable_answers_test.py
+++ tests/acceptable_answers_test.py
@@ -8,7 +8,7 @@ class TestAcceptableAnswers(TestCase):
     def setUp(self):
         self.qa = Interface()
 
-    @requirements(['#0001', '#0002'])
+    @requirements(['#0001', '#0002', '#0003'])
     def test_ask_invalidKeyword(self):
         answer = self.qa.ask('Is anyone there>')
         self.assertEqual('Was that a question?', answer)
@@ -29,18 +29,48 @@ class TestAcceptableAnswers(TestCase):
         self.assertEqual('Was that a question?', answer)
 
     @requirements(['#0006'])
-    def test_ask_90percentValidInitialAnswer5_matches(self):
-        pass
+    def test_ask_90percentMatch_InitialAnswer5_matches(self):
+        answer = self.qa.ask('Why dun\'t yu undrstnd me>')
+        self.assertEqual('Because you do not speak 1s and 0s', answer)
 
     @requirements(['#0006'])
-    def test_ask_80percentInitialAnswer5_noMatch(self):
-        answer = self.qa.ask('Why don\'t you understand>')
+    def test_ask_below90percent_InitialAnswer5_noMatch(self):
+        answer = self.qa.ask('Why dun\'t you understnd>')
         self.assertEqual('I don\'t know, please provide the answer', answer)
 
     @requirements(['#0007', '#0019'])
     def test_ask_excludeNumbersFromValid(self):
-        answer = self.qa.ask('Who invented Python 2.7>')
-        self.assertEqual('Guido Rossum(BFDL)', answer)
+        answer = self.qa.ask('Why don\'t you understand me 2 2 2 2 2 2>')
+        self.assertEqual('Because you do not speak 1s and 0s', answer)
+
+    @requirements(['#0010', '#0012'])
+    def test_teach_noPreviousQuestion(self):
+        response = self.qa.teach("Wolfgang Amadeus Mozart");
+        self.assertEqual('Please ask a question first', response)
+
+    @requirements(['#0009', '#0010', '#0012'])
+    def test_teach_previousQuestion_learns(self):
+        answer = self.qa.ask('Who was the most prolific classical composer>')
+        self.assertEqual('I don\'t know, please provide the answer', answer)
+        self.qa.teach("Wolfgang Amadeus Mozart");
+        answer = self.qa.ask('Who was the most prolific classical composer>')
+        self.assertEqual('Wolfgang Amadeus Mozart', answer)
+
+    @requirements(['#0010', '#0013'])
+    def test_teach_alreadyAnswered(self):
+        answer = self.qa.ask('Who was the most prolific classical composer>')
+        self.assertEqual('I don\'t know, please provide the answer', answer)
+        self.qa.teach("Wolfgang Amadeus Mozart");
+        answer = self.qa.ask('Who was the most prolific classical composer>')
+        self.assertEqual('Wolfgang Amadeus Mozart', answer)
+        response = self.qa.teach("Ludwig Van Beethoven")
+        self.assertEqual('I don\'t know about that. I was taught differently', response)
+
+    @requirements(['#0010', '#0011'])
+    def test_teach_numericAnswer(self):
+        answer = self.qa.ask('What year was Mozart born>')
+        self.assertEqual('I don\'t know, please provide the answer', answer)
+        self.qa.teach(1756)
 
     @requirements(['#0008', '#0017'])
     def test_ask_validQuestion_initialAnswer1(self):
@@ -61,11 +91,11 @@ class TestAcceptableAnswers(TestCase):
         self.assertEqual('Guido Rossum(BFDL)', answer)
 
     @requirements(['#0008', '#0020'])
-    def test_ask_validQuestion_initialAnswer3(self):
+    def test_ask_validQuestion_initialAnswer4(self):
         answer = self.qa.ask('Why don\'t you understand me>')
         self.assertEqual('Because you do not speak 1s and 0s', answer)
 
     @requirements(['#0008', '#0021'])
-    def test_ask_validQuestion_initialAnswer3(self):
+    def test_ask_validQuestion_initialAnswer5(self):
         answer = self.qa.ask('Why don\'t you shutdown>')
         self.assertEqual('I\'m afraid I can\'t do that {0}'.format(getpass.getuser()), answer)
