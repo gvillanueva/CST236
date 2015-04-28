2bdcd2c9f53ad2c7d3abf1839d3a42e3c155e4f8
Giancarlo Villanueva <villanueva.giancarlo@gmail.com>
2015-04-27 17:11:49 -0700

    Requirements fully tested.

--- tests/acceptable_answers_test.py
+++ tests/acceptable_answers_test.py
@@ -2,7 +2,13 @@ from pyTona.main import Interface
 from ReqTracer import requirements
 from unittest import TestCase
 import getpass
+from datetime import datetime, date, time
 
+def inchesToMillimetersWrong(inches):
+    return inches * 4.52
+
+def inchesToMillimeters(inches):
+    return inches * 25.4
 
 class TestAcceptableAnswers(TestCase):
     def setUp(self):
@@ -43,12 +49,12 @@ class TestAcceptableAnswers(TestCase):
         answer = self.qa.ask('Why don\'t you understand me 2 2 2 2 2 2>')
         self.assertEqual('Because you do not speak 1s and 0s', answer)
 
-    @requirements(['#0010', '#0012'])
+    @requirements(['#0010', '#0011', '#0012'])
     def test_teach_noPreviousQuestion(self):
         response = self.qa.teach("Wolfgang Amadeus Mozart");
         self.assertEqual('Please ask a question first', response)
 
-    @requirements(['#0009', '#0010', '#0012'])
+    @requirements(['#0009', '#0010', '#0011', '#0012'])
     def test_teach_previousQuestion_learns(self):
         answer = self.qa.ask('Who was the most prolific classical composer>')
         self.assertEqual('I don\'t know, please provide the answer', answer)
@@ -56,7 +62,7 @@ class TestAcceptableAnswers(TestCase):
         answer = self.qa.ask('Who was the most prolific classical composer>')
         self.assertEqual('Wolfgang Amadeus Mozart', answer)
 
-    @requirements(['#0010', '#0013'])
+    @requirements(['#0009', '#0010', '#0011', '#0013'])
     def test_teach_alreadyAnswered(self):
         answer = self.qa.ask('Who was the most prolific classical composer>')
         self.assertEqual('I don\'t know, please provide the answer', answer)
@@ -66,11 +72,53 @@ class TestAcceptableAnswers(TestCase):
         response = self.qa.teach("Ludwig Van Beethoven")
         self.assertEqual('I don\'t know about that. I was taught differently', response)
 
+    @requirements(['#0009', '#0010', '#0011'])
+    def test_teach_functionPointer(self):
+        answer = self.qa.ask('What is 1 inch in millimeters>')
+        self.assertEqual('I don\'t know, please provide the answer', answer)
+        self.qa.teach(inchesToMillimeters)
+        answer = self.qa.ask('What is 1 inch in millimeters>')
+        self.assertEqual(25.4, answer)
+
     @requirements(['#0010', '#0011'])
     def test_teach_numericAnswer(self):
         answer = self.qa.ask('What year was Mozart born>')
         self.assertEqual('I don\'t know, please provide the answer', answer)
         self.qa.teach(1756)
+        answer = self.qa.ask('What year was Mozart born>')
+        self.assertEqual(1756, answer)
+
+    @requirements(['#0014', '#0015', '#0016'])
+    def test_correct_noPreviousQuestion(self):
+        response = self.qa.teach('Ludwig Van Beethoven')
+        self.assertEqual('Please ask a question first', response)
+
+    @requirements(['#0014', '#0015'])
+    def test_correct_alreadyAnswered(self):
+        self.qa.ask('Who was the most prolific classical composer>')
+        self.qa.teach("Wolfgang Amadeus Mozart")
+        self.qa.correct("Ludwig Van Beethoven")
+        answer = self.qa.ask('Who was the most prolific classical composer>')
+        self.assertEqual('Ludwig Van Beethoven', answer)
+
+    @requirements(['#0014', '#0015'])
+    def test_correct_numericAnswer_invalid(self):
+        self.qa.ask('What year was Mozart born>')
+        self.qa.teach('1752')
+        self.qa.correct(1756)
+        answer = self.qa.ask('What year was Mozart born>')
+        self.assertEqual(1756, answer)
+
+    @requirements(['#0009', '#0014', '#0015'])
+    def test_correct_functionPointer(self):
+        answer = self.qa.ask('What is 1 inch in millimeters>')
+        self.assertEqual('I don\'t know, please provide the answer', answer)
+        self.qa.teach(inchesToMillimetersWrong)
+        answer = self.qa.ask('What is 1 inch in millimeters>')
+        self.assertEqual(4.52, answer)
+        self.qa.correct(inchesToMillimeters)
+        answer = self.qa.ask('What is 1 inch in millimeters>')
+        self.assertEqual(25.4, answer)
 
     @requirements(['#0008', '#0017'])
     def test_ask_validQuestion_initialAnswer1(self):
@@ -81,9 +129,9 @@ class TestAcceptableAnswers(TestCase):
 
     @requirements(['#0008', '#0018'])
     def test_ask_validQuestion_initialAnswer2(self):
-        # answer = self.qa.ask('How many seconds since 5/4/1986>')
-        # self.assertEqual(dateTime, answer)
-        pass
+        answer = self.qa.ask('How many seconds since 12>')
+        result = datetime.now() - datetime.combine(date.today(), time(12))
+        self.assertEqual(result.seconds, answer)
 
     @requirements(['#0008', '#0019'])
     def test_ask_validQuestion_initialAnswer3(self):
